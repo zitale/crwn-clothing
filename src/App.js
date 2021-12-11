7671352a227d58810a44e04017519943d6a81c80
zitale <zito.ales@gmail.com>
2021-12-11 22:59:13 +0100

    some basic navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
 
+import {
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = props => {
+  const { hatId } = useParams();
+  return (
+    <div>
+      <Link to='/'>Home</Link>
+      <h1>HATS PAGE AT {hatId}</h1>
+    </div>)
+}
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+       <Routes>
+         <Route path='/' element={<HomePage />}/>
+         <Route path='/hats' element={<HatsPage />} />
+         <Route path='/hats/:hatId' element={<HatsPage />} />
+       </Routes>
+    </div>
   );
 }
 
