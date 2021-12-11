53e1c68482282d728ace983d90be28fcb4dbddad
zitale <zito.ales@gmail.com>
2021-12-11 12:19:36 +0100

    first commit crwn-clothes

---
+++ src/homepage.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './homepage.style.scss'
+
+
+const HomePage = () => (
+  <div className='homepage'>
+      <div className='directory-menu'>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className='title'> PRODOTTO 1</h1>
+                  <span className='subtitle'>SHOP NOW</span>
+              </div>
+          </div>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className='title'> PRODOTTO 2</h1>
+                  <span className='subtitle'>SHOP NOW</span>
+              </div>
+          </div>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className='title'> PRODOTTO 3</h1>
+                  <span className='subtitle'>SHOP NOW</span>
+              </div>
+          </div>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className='title'> PRODOTTO 4</h1>
+                  <span className='subtitle'>SHOP NOW</span>
+              </div>
+          </div>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className='title'> PRODOTTO 5</h1>
+                  <span className='subtitle'>SHOP NOW</span>
+              </div>
+          </div>
+
+      </div>
+  </div>
+)
+
+
+export default HomePage;
