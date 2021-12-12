9b50b5503727761aab5c376d425da7a99c0bd959
zitale <zito.ales@gmail.com>
2021-12-12 16:11:26 +0100

    sjop page and components

--- src/App.js
+++ src/App.js
@@ -4,28 +4,17 @@ import './App.css';
 import {
   Routes,
   Route,
-  Link,
-  useParams,
 } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = props => {
-  const { hatId } = useParams();
-  return (
-    <div>
-      <Link to='/'>Home</Link>
-      <h1>HATS PAGE AT {hatId}</h1>
-    </div>)
-}
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
        <Routes>
          <Route path='/' element={<HomePage />}/>
-         <Route path='/hats' element={<HatsPage />} />
-         <Route path='/hats/:hatId' element={<HatsPage />} />
+         <Route path='/shop' element={<ShopPage />} />
        </Routes>
     </div>
   );
