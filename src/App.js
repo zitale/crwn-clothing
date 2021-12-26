9628dbe27d1819132e2d7eec71e78ac55816e6e0
zitale <zito.ales@gmail.com>
2021-12-26 17:47:58 +0100

    signup with emailandpassword to db

--- src/App.js
+++ src/App.js
@@ -8,16 +8,62 @@ import {
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInPage from "./pages/signinpage/signinpage.component";
 
-function App() {
-  return (
-    <div>
-       <Routes>
-         <Route path='/' element={<HomePage />}/>
-         <Route path='/shop' element={<ShopPage />} />
-       </Routes>
-    </div>
-  );
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot( snapShot => {
+          this.setState({
+            currentUser:{
+                      id: snapShot.id,
+                      ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({currentUser:userAuth})
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/shop' element={<ShopPage/>}/>
+          <Route path='/signin' element={<SignInPage/>}/>
+        </Routes>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+
