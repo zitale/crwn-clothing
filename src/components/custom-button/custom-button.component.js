29e90c39b55e486e20498b9187ee2deed4c2a86e
zitale <zito.ales@gmail.com>
2021-12-28 13:29:47 +0100

    add items with quantitz in cart redux

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -3,8 +3,8 @@ import React from "react";
 import './custom-button.style.scss';
 
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-  <button className={`${isGoogleSignIn? 'google-sing-in': '' } custom-button`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+  <button className={`${inverted? 'inverted': '' } ${isGoogleSignIn? 'google-sing-in': '' } custom-button`} {...otherProps}>
     {children}
   </button>
 )
